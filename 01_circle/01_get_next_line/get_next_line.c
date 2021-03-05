8279388b6d47dc8fed1a5efa22ce26cc9b2dd427
chanhohan <chhqm2759@naver.com>
2021-03-05 20:59:30 +0900

    [ADD} get_next_line [MOD}circle name

---
+++ 01_circle/01_get_next_line/get_next_line.c
@@ -0,0 +1,110 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chan <chan@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/13 15:31:22 by chan              #+#    #+#             */
+/*   Updated: 2021/03/05 12:50:15 by chan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	nl_split(int *ind, char **save, char **line)
+{
+	char	*r;
+	int		i;
+
+	i = 1;
+	(*save)[*ind] = '\0';
+	if (!(*line = ft_strdup(*save)))
+		return ;
+	r = NULL;
+	if ((*save)[*ind + i])
+		r = ft_strdup(*save + *ind + i);
+	free(*save);
+	*save = r;
+	*ind = 0;
+}
+
+void	ft_gnljoin(char **save, char *buff)
+{
+	char	*tmp;
+
+	if (!(tmp = ft_strjoin(*save, buff)))
+		return ;
+	if (*save && tmp)
+	{
+		free(*save);
+		*save = NULL;
+		*save = tmp;
+	}
+}
+
+int		nl_operation(int *ind, char **save, char **line)
+{
+	if (!(*save))
+		return (1);
+	while ((*save)[*ind] && (*save)[*ind] != '\n')
+		(*ind)++;
+	if ((*save)[*ind] != '\n')
+		return (1);
+	nl_split(ind, save, line);
+	return (0);
+}
+
+int		exception_check(char **save, int num_line, char **line)
+{
+	int	i;
+
+	i = 0;
+	if (num_line == -1)
+		return (ERROR);
+	if (num_line > 0)
+		return (LINE_R);
+	if (!(*save) && !num_line)
+	{
+		*line = ft_strdup("");
+		return (_EOF);
+	}
+	while ((*save)[i] && (*save)[i] != '\n')
+		i++;
+	if (!(*save)[i])
+	{
+		*line = ft_strdup(*save);
+		free(*save);
+		*save = NULL;
+		return (_EOF);
+	}
+	if (!nl_operation(&i, save, line))
+		return (LINE_R);
+	return (_EOF);
+}
+
+int		get_next_line(int fd, char **line)
+{
+	static char	*save[OPEN_MAX];
+	char		*buff;
+	ssize_t		num_line;
+	int			i;
+
+	i = 0;
+	num_line = 1;
+	if (fd < 0 || fd >= OPEN_MAX || !line || BUFFER_SIZE <= 0 ||
+		!(buff = (char *)malloc(BUFFER_SIZE + 1)))
+		return (ERROR);
+	while (nl_operation(&i, &save[fd], line) &&
+			(num_line = read(fd, buff, BUFFER_SIZE)) > 0)
+	{
+		buff[num_line] = '\0';
+		if (save[fd])
+			ft_gnljoin(&save[fd], buff);
+		else
+			save[fd] = ft_strdup(buff);
+	}
+	free(buff);
+	buff = NULL;
+	return (exception_check(&save[fd], num_line, line));
+}
---
+++ 01_circle/01_get_next_line/get_next_line.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chan <chan@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/13 15:32:06 by chan              #+#    #+#             */
+/*   Updated: 2021/02/26 13:28:05 by chan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# define ERROR -1
+# define _EOF 0
+# define LINE_R 1
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 4096
+# endif
+
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char *s1, char const *s2);
+int		get_next_line(int fd, char **line);
+void	nl_split(int *ind, char **arr, char **line);
+int		nl_operation(int *ind, char **arr, char **line);
+void	ft_gnljoin(char **save, char *buff);
+int		exception_check(char **save, int num_line, char **line);
+size_t	ft_strlen(const char *s);
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
+size_t	ft_strlcat(char *dest, const char *src, size_t n);
+
+#endif
