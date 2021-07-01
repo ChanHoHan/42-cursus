181e3445fa6ed93040e62b5e7ca1cff8e5a1431c
chanhohan <chhqm2759@naver.com>
2021-07-01 15:59:48 +0900

    [MOD] docker file

--- 02_circle/ft_server/srcs/run.sh
+++ 02_circle/ft_server/srcs/run.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-service nginx start
-service php7.3-fpm start
 service mysql start
+service php7.3-fpm start
+service nginx start
+
+if [ ${AUTOINDEX} -eq -1 ]; then
+        sed -i 's/autoindex on;/autoindex off;/g' /etc/nginx/sites-available/default
+fi
+bash
