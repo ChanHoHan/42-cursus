3037db72d8cf28bd4a7d198b421f48e6b71e1e9e
chanhohan <chhan2759@gmail.com>
2021-07-02 14:01:04 +0900

    final

--- 02_circle/ft_server/srcs/run.sh
+++ 02_circle/ft_server/srcs/run.sh
@@ -16,7 +16,7 @@ service mysql start
 service php7.3-fpm start
 service nginx start
 
-if [ ${AUTOINDEX} -eq -1 ]; then
+if [ ${AUTOINDEX} == -1 ]; then
         sed -i 's/autoindex on;/autoindex off;/g' /etc/nginx/sites-available/default
 fi
 bash
